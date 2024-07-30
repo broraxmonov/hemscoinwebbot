493e01a47dd82105bef273af8715b21032998bb9
broraxmonov <bro.raxmonov@gmail.com>
2024-07-31 02:37:49 +0500

    push

---
+++ src/images/index.ts
@@ -0,0 +1,21 @@
+import hamsterExchange from "./hamster-exchange.png";
+import binanceLogo from "./hemscoin.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./wallet.jpg";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./rem.png";
+import hamsterCoin from "./hemscoin.png";
+import wallet from "./wallet.jpg";
+
+
+export {
+    hamsterExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    hamsterCoin
+};
